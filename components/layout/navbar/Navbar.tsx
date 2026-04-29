a296bfc6531f0b6dee88e79453b49a2e35d614c3
Hardy Lino <harv2222@gmail.com>
2026-04-29 18:46:29 +0200

    navbar added

--- components/layout/navbar/Navbar.tsx
+++ components/layout/navbar/Navbar.tsx
@@ -1,72 +1,57 @@
-'use client'
+'use client';
 
-import { AppleMusic } from '@/components/icons/AppleMusic'
-import { Facebook } from '@/components/icons/Facebook'
-import { Instagram } from '@/components/icons/Instagram'
-import { LutaLogo } from '@/components/icons/logo/LutaLogo'
-import { Spotify } from '@/components/icons/Spotify'
-import { WeezerIcon } from '@/components/icons/WeezerIcon'
-import { Youtube } from '@/components/icons/Youtube'
-import Link from 'next/link'
-import styles from './Navbar.module.scss'
+import { AppleMusic } from '@/components/icons/AppleMusic';
+import { Facebook } from '@/components/icons/Facebook';
+import { Instagram } from '@/components/icons/Instagram';
+import { LutaLogo } from '@/components/icons/logo/LutaLogo';
+import { Spotify } from '@/components/icons/Spotify';
+import { WeezerIcon } from '@/components/icons/WeezerIcon';
+import { Youtube } from '@/components/icons/Youtube';
+import Link from 'next/link';
+import styles from './Navbar.module.scss';
 
 const navLinks = [
   { href: '#accueil', label: 'ACCUEIL' },
   { href: '/bio', label: 'BIO' },
   { href: '#musique', label: 'MUSIQUE' },
   { href: '#contact', label: 'CONTACT' },
-]
+];
 
 const socialLinks = [
   {
     href: '#',
     label: 'Instagram',
-    icon: (
-      <Instagram width={32}/>
-    ),
+    icon: <Instagram width={32} />,
   },
   {
     href: '#',
     label: 'Facebook',
-    icon: (
-      <Facebook width={30}/>
-    ),
+    icon: <Facebook width={30} />,
   },
   {
     href: '#',
     label: 'YouTube',
-    icon: (
-     <Youtube width={30}/>
-    ),
+    icon: <Youtube width={30} />,
   },
   {
     href: '#',
     label: 'Apple Music',
-    icon: (
-      <AppleMusic width={30}/>
-    ),
+    icon: <AppleMusic width={30} />,
   },
   {
     href: '#',
     label: 'Weezer',
-    icon: (
-      <WeezerIcon width={30}/>
-    ),
+    icon: <WeezerIcon width={30} />,
   },
   {
     href: '#',
     label: 'Spotify',
-    icon: (
-      <Spotify width={30}/>
-    ),
+    icon: <Spotify width={30} />,
   },
-]
+];
 
 function LogoMark() {
-  return (
-    
-    <LutaLogo width={80}/>
-  )
+  return <LutaLogo width={80} />;
 }
 
 export default function Navbar() {
@@ -85,7 +70,13 @@ export default function Navbar() {
 
       <div className={styles.social}>
         {socialLinks.map(({ href, label, icon }) => (
-          <a key={label} href={href} target="_blank" rel="noopener noreferrer" aria-label={label}>
+          <a
+            key={label}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={label}
+          >
             {icon}
           </a>
         ))}
@@ -101,5 +92,5 @@ export default function Navbar() {
         ))}
       </ul>
     </nav>
-  )
+  );
 }
