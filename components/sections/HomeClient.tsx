9447995735ec76a4790ac3ab1cff9a2e9c9d3d2f
Hardy Lino <harv2222@gmail.com>
2026-05-04 19:21:04 +0200

    hero hover  animation added

--- components/sections/HomeClient.tsx
+++ components/sections/HomeClient.tsx
@@ -43,20 +43,25 @@ export function HomeClient({ name, description }: HomeClientProps) {
 
         <main className="">
           <section id="hero" className=" h-screen">
-            <div className="w-full h-auto flex ">
-              <div className="relative w-full h-full flex flex-col items-center justify-center pt-120">
-                <h1 className="sr-only">Luta Musique</h1>
-                <div className="absolute top-44  flex flex-row justify-center  w-full h-auto gap-10 pl-64">
-                  <IntroImage
-                    className="pt-4"
-                    hoverSrc={revealProps.src}
-                    brushSize={0.65}
-                    trailDecay={2.5}
-                    smoothness={0.95}
-                  />
-                  <SubText className="" />
+            <div className="w-full h-full">
+              <div className="relative w-full h-full flex flex-col items-center justify-center ">
+                {/* contenedor en full */}
+                <div className="h-full w-full flex flex-col  justify-center items-center">
+                  <h1 className="sr-only">Luta Musique</h1>
+                  {/**primer bloque para col */}
+                  <div className="relative w-full flex-1">
+                    <IntroImage
+                      className="absolute inset-0 m-auto translate-y-55"
+                      hoverSrc={revealProps.src}
+                      brushSize={0.65}
+                      trailDecay={2.5}
+                      smoothness={0.95}
+                    />
+                    <SubText className="relative left-[65%] top-[58%] pointer-events-none" />
+                  </div>
+                  {/**segundo bloque para el col */}
+                  <HeroText className="text-center  flex-1  pt-40" />
                 </div>
-                <HeroText className="text-center " />
               </div>
             </div>
           </section>
@@ -64,4 +69,25 @@ export function HomeClient({ name, description }: HomeClientProps) {
       </div>
     </>
   );
+  {
+    /**
+          
+           <section id="hero" className=" h-screen">
+            <div className="w-full h-auto flex ">
+              <div className="relative w-full h-full flex flex-col items-center justify-center pt-120">
+                <h1 className="sr-only">Luta Musique</h1>
+                <div className="absolute top-44  flex flex-row justify-center  w-full h-auto gap-10 ">
+                  <IntroImage
+                    className="pt-4"
+                    hoverSrc={revealProps.src}
+                    brushSize={0.65}
+                    trailDecay={2.5}
+                    smoothness={0.95}
+                  />
+                </div>
+                <HeroText className="text-center " />
+              </div>
+            </div>
+          </section>*/
+  }
 }
